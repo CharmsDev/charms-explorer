4fa2e48b5a9af21bb4499302237d1b0d4402c8a6
Ricart <93732312+capitaharlock@users.noreply.github.com>
2025-05-11 13:31:21 +0200

    Indexer update + cleanup + remove env hardcodes + remove diagnose

--- indexer/migration/src/lib.rs
+++ indexer/migration/src/lib.rs
@@ -2,6 +2,7 @@ pub use sea_orm_migration::prelude::*;
 
 mod m20250415_000001_create_tables;
 mod m20250415_000002_add_status_fields;
+mod m20250508_000001_add_timestamp_to_bookmark;
 
 pub struct Migrator;
 
@@ -11,6 +12,7 @@ impl MigratorTrait for Migrator {
         vec![
             Box::new(m20250415_000001_create_tables::Migration),
             Box::new(m20250415_000002_add_status_fields::Migration),
+            Box::new(m20250508_000001_add_timestamp_to_bookmark::Migration),
         ]
     }
 }
