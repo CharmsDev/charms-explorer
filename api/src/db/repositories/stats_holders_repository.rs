2f31fe8f15106f657b2dc0ffe97ffbfaaea7dc30
Ricart <93732312+capitaharlock@users.noreply.github.com>
2025-11-26 22:46:30 +0100

    Revert "refactor(api): remove internal development tags from comments"
    This reverts commit 2da1ab49f389664ef15bf88c42f2fece09040ac8.

--- api/src/db/repositories/stats_holders_repository.rs
+++ api/src/db/repositories/stats_holders_repository.rs
@@ -1,4 +1,4 @@
-// Repository for stats_holders table operations
+// [RJJ-STATS-HOLDERS] Repository for stats_holders table operations
 use crate::db::DbError;
 use crate::entity::stats_holders;
 use sea_orm::*;
